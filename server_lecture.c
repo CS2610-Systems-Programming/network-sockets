d7d33053ade196feaf6eac84a6b6d2893f7425a3
Chau Nguyen Developer <chau.nguyen.dev@gmail.com>
2025-04-30 15:32:04 -0700

    Continue creating server side of the chat app. Had a lot of questions for professor Lucero.

--- server_lecture.c
+++ server_lecture.c
@@ -1,5 +1,10 @@
 //Create sockets for the server.
 #include <sys/socket.h>
+#define PortNumber	1234
+#define MaxConnects	8
+#define BuffSize	256
+#define ConversationLen	3
+#define Host		"localhost"
 
 int main()
 {
@@ -26,10 +31,24 @@ int main()
 	//It holds info in regards to the address family, port, and IP address that a socket will use
 	//when binding to a network address or when connecting to a remote server. 
 	
+	//This structure holds IP address and port information for IPv4 sockets.
 	struct sockaddr_in socketAddress;
+
 	//Initialize (clear) the socketAddress structure by setting all its bytes to zero.
 	//memset() is a standard C library function that fills a block of memory with specific value.
 	memset(&socketAddress, 0, sizeof(socketAddress));
+
+	//Tell the socket API what kind of addresses I am using
+	socketAddress.sin_family = AF_INET;
+
+	//host-to-network-LONG: Converts the IP address to network byte order
+	//I am trying to binding the socket to all available network interfaces on my machine.
+	//(including localhost, Wifi, Ethernet, etc.)
 	
+	socketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	//
+	socketAddress.sin_port = htons(PortNumber)
+			
 	
 }
